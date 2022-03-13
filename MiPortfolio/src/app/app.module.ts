c932e852fb7d1c72c97fc251424bd8da6cba2daf
crazyro15 <crazyro15@hotmail.com>
2022-03-13 19:37:39 -0300

    acerca-de

--- MiPortfolio/src/app/app.module.ts
+++ MiPortfolio/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
